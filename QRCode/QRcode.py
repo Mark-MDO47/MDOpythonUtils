1b74c99f0d3a0f8a9891840de7a408446449dcba
Mark-MDO47 <mdo@ieee.org>
2024-12-23 14:10:01 -0800

    QRcode.py add parameters

--- QRCode/QRcode.py
+++ QRCode/QRcode.py
@@ -12,7 +12,7 @@ import argparse
 
 ###################################################################################
 # do_QRcode() - read instructions from fname; make QR code files
-def do_QRcode(fname):
+def do_QRcode(fname, debug_output, split_text):
     fmt = '<img src="%s" width="400" alt="%s" style="max-width: 100%s;"><br><br>\n\n\n'
     my_cwd = os.getcwd()
 
@@ -23,7 +23,8 @@ def do_QRcode(fname):
         # process the line for validity checking
         line_good = True
         a_line = a_line.strip()
-        sys.stderr.write("%s\n" % a_line)
+        if debug_output:
+            sys.stderr.write("%s\n" % a_line)
         a_split_tmp = a_line.split("\t")
         a_split = []
         for t in a_split_tmp:
@@ -31,13 +32,13 @@ def do_QRcode(fname):
 
         # validity check the line
         if (3 > len(a_split)):
-            sys.stdout.write("  $$$ ERROR $$$: instruction must have 3 tab-separated fields, not %d<br><br>\n\n" % len(a_split))
+            sys.stderr.write("  $$$ ERROR $$$: instruction must have 3 tab-separated fields, not %d<br><br>\n\n" % len(a_split))
             line_good = False
         elif 4 != (len(a_split[0]) - a_split[0].find(".png")):
-            sys.stdout.write("  $$$ ERROR $$$: QR filename must be *.png not '%s'<br><br>\n\n" % a_split[0])
+            sys.stderr.write("  $$$ ERROR $$$: QR filename must be *.png not '%s'<br><br>\n\n" % a_split[0])
             line_good = False
         elif (-1 != a_split[0].find("\\")) or (-1 != a_split[0].find("/")):
-            sys.stdout.write("  $$$ ERROR $$$: QR filename not change directories as in '%s'<br><br>\n\n" % a_split[0])
+            sys.stderr.write("  $$$ ERROR $$$: QR filename not change directories as in '%s'<br><br>\n\n" % a_split[0])
             line_good = False
 
         if line_good:        
@@ -45,7 +46,9 @@ def do_QRcode(fname):
             img = qrcode.make(a_split[2]) # [2] is text for QR code
             img.save(a_split[0])          # [0] is filename
             fout = open(a_split[0][:-3] + "html", 'wt')
-            tmp = a_split[2].find("|")
+            tmp = -1
+            if split_text:
+                tmp = a_split[2].find("|")
             fout.write("<H1>%s : %s</H1><br><br>\n" % (a_split[1], a_split[2][tmp+1:]))
             fout.write(fmt % (a_split[0],a_split[1],"%"))
             fout.close()
@@ -73,6 +76,8 @@ python QRcode.py instructions.txt
 """,
         usage='%(prog)s instructions')
     my_parser.add_argument('instructions',type=str,help='path to instructions.txt file')
+    my_parser.add_argument('-d', '--debug_output', action='store_true', help='enable debug output')
+    my_parser.add_argument('-s', '--split_text', action='store_true', help='enable split text on |')
     args = my_parser.parse_args()
 
-    do_QRcode(args.instructions)
+    do_QRcode(args.instructions, args.debug_output, args.split_text)
